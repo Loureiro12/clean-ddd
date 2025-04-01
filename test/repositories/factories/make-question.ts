eeea6ffad39ec0b4f80f05460bdab1e56dcb9366
André Loureiro <contato.loureiro1@gmail.com>
2025-04-01 17:13:20 -0300

    feat: creating factors for the tests

---
+++ test/repositories/factories/make-question.ts
@@ -0,0 +1,24 @@
+import { faker } from '@faker-js/faker'
+
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import {
+  Question,
+  QuestionProps,
+} from '@/domain/forum/enterprise/entities/question'
+
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      authorId: new UniqueEntityID('author-1'),
+      ...override,
+    },
+    id,
+  )
+
+  return question
+}
